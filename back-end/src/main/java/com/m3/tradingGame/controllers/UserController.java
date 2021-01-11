bf2b458d0ec2aae7d37ad22bc07c457747199d4e
Hennnnrryyy <henrysmithjr@me.com>
2021-01-11 12:32:06 -0500

    Edited UserController to follow SpringBoot REST

--- back-end/src/main/java/com/m3/tradingGame/controllers/UserController.java
+++ back-end/src/main/java/com/m3/tradingGame/controllers/UserController.java
@@ -1,12 +1,23 @@
 package com.m3.tradingGame.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.m3.tradingGame.dao.ItemDao;
+import com.m3.tradingGame.dao.UserDao;
+import com.m3.tradingGame.entities.User;
+
+import smith.app.model.ToDo;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
@@ -16,41 +27,34 @@ public class UserController {
 	ItemDao itemDao;
 	
 	
-	@GetMapping("users")
+	@GetMapping
 	public String getAllUsers() {
 		
 		return "users";
 	}
 	
-	@GetMapping("userDetail")
+	@PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User create(@RequestBody User u) {
+        return userDao.add(u);
+    }
+	
+	@GetMapping("/{id}")
 	public String getUserInfo(Integer id, Model model) {
 		
 		return "userDetail";
 	}
 	
-	@PostMapping("addUser")
-	public String addUser(){
-		
-		return "redirect:/users";
-	}
-	
-	@GetMapping("deleteUser")
+	@GetMapping("/{id}")
 	public String deleteUser() {
 		
-		return "redirect:/users";
+		return "deleted user";
 	}
 	
-	@GetMapping("editUser")
+	@GetMapping("/{id}")
 	public String editUser() {
 		
-		return "editUser";
+		return "edited user";
 	}
-	
-	@PostMapping("editStudent")
-	public String performEditUser() {
 		
-		return "redirect:/users";
-	}
-	
-	
 }
