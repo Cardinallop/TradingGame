7da719fb54a2712b7366ede485d972b39688b665
brockcheese <broqpace@gmail.com>
2021-01-11 16:04:02 -0600

    New Test

--- back-end/src/test/java/com/m3/tradingGame/dao/UserDaoDBTest.java
+++ back-end/src/test/java/com/m3/tradingGame/dao/UserDaoDBTest.java
@@ -7,6 +7,8 @@ package com.m3.tradingGame.dao;
 
 import com.m3.tradingGame.entities.Item;
 import com.m3.tradingGame.entities.User;
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -64,7 +66,28 @@ public class UserDaoDBTest {
      * Test of getUserById method, of class UserDaoDB.
      */
     @Test
-    public void testGetUserById() {
+    public void testAddAndGetUser() {
+        Item item = new Item();
+        item.setName("Test Name");
+        item.setInitialPrice(new BigDecimal(5.00));
+        item.setCurrentPrice(new BigDecimal(10.00));
+        item = itemDao.addItem(item);
+        
+        HashMap<Item, Integer> items = new HashMap<>();
+        items.put(item, 3);
+        
+        User user = new User();
+        user.setUsername("brockcheese");
+        user.setFirstName("Brock");
+        user.setLastName("Pace");
+        user.setPassword("goo");
+        user.setMoney(new BigDecimal(35.69));
+        user.setDifficulty("hard");
+        user.setItems(items);
+        user = userDao.addUser(user);
+        
+        User fromDao = userDao.getUserById(user.getId());
+        assertEquals(user, fromDao);
     }
 
     /**
