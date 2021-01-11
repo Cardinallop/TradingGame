ffc08cdb39458525bfe55fb4214383473cf82f48
brockcheese <broqpace@gmail.com>
2021-01-11 10:26:19 -0600

    moved user entity

--- back-end/src/main/java/com/m3/tradingGame/entities/User.java
+++ back-end/src/main/java/com/m3/tradingGame/entities/User.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.m3.entities;
+package com.m3.tradingGame.entities;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
