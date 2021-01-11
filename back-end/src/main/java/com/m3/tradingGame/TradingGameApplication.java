87176c936263d545d0951c0087adfba05303687b
ann-grg <ann.grg@hotmail.com>
2021-01-11 13:32:46 +0000

    Back-end + amazon rds setup

---
+++ back-end/src/main/java/com/m3/tradingGame/TradingGameApplication.java
@@ -0,0 +1,13 @@
+package com.m3.tradingGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingGameApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/m3/tradingGame/TradingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m3.tradingGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
