cfd1fa9358787ae1e6f8b50255e2494011a2c98b
Hennnnrryyy <henrysmithjr@me.com>
2021-01-11 12:02:24 -0500

    Added more skeleton code for user controller

--- back-end/src/main/java/com/m3/tradingGame/controllers/UserController.java
+++ back-end/src/main/java/com/m3/tradingGame/controllers/UserController.java
@@ -1,8 +1,56 @@
 package com.m3.tradingGame.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 public class UserController {
-
+	
+	@Autowired
+	UserDao userDao;
+	
+	@Autowired
+	ItemDao itemDao;
+	
+	
+	@GetMapping("users")
+	public String getAllUsers() {
+		
+		return "users";
+	}
+	
+	@GetMapping("userDetail")
+	public String getUserInfo(Integer id, Model model) {
+		
+		return "userDetail";
+	}
+	
+	@PostMapping("addUser")
+	public String addUser(){
+		
+		return "redirect:/users";
+	}
+	
+	@GetMapping("deleteUser")
+	public String deleteUser() {
+		
+		return "redirect:/users";
+	}
+	
+	@GetMapping("editUser")
+	public String editUser() {
+		
+		return "editUser";
+	}
+	
+	@PostMapping("editStudent")
+	public String performEditUser() {
+		
+		return "redirect:/users";
+	}
+	
+	
 }
